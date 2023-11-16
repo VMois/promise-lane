db571d2486e6232bff6ffdd4e2ebf8e1cfe8c389
VMois <vmois@protonmail.com>
2023-11-17 00:47:24 +0100

    Only execute _run when there is something in queue

--- src/index.js
+++ src/index.js
@@ -4,37 +4,39 @@ export default class PromiseLane {
     constructor(concurrency) {
         this.concurrency = concurrency;
         this.current = 0
-        this.stopRun = false;
 
-        // TODO: ArrayList is slow for dequeue operations, better LinkedList
+        // TODO: ArrayList is slow for dequeue operations, better to use LinkedList
         this.queue = new Array();
 
         this.boundRun = this._run.bind(this);
-        this.boundRun();
     }
 
     add(factory) {
         return new Promise((resolve, reject) => {
             this.queue.push([resolve, reject, factory])
+            this.boundRun();
         })
-    }
-
-    stop() {
-        this.stopRun = true
     }
 
     _run() {
         while (this.current < this.concurrency && this.queue[0] !== undefined) {
-            let [resolve, reject, factory] = this.queue.shift()
+            let [resolve, reject, factory] = this.queue.shift();
             this.current++
 
+            // Run a factory, get a promise and execute it
             factory()
             .then((value) => resolve(value))
             .catch((error) => reject(error))
-            .finally(() => this.current--)
+            .finally(() => this.current--);
         }
-        if (!this.stopRun) {
-            setTimeout(this.boundRun, 0);
+
+        // If no items are in a queue, 
+        // no need to send _run at the end of event loop
+        if (this.queue[0] === undefined) {
+            return;
         }
+        
+        // Calls check funct
+        setTimeout(this.boundRun, 0);
     }
 }
--- test/spec.js
+++ test/spec.js
@@ -8,13 +8,10 @@ const p2 = () => new Promise((resolve, reject) => {
     setTimeout(resolve, 100, 'world')
 });
 
-const promiseLane = new PromiseLane(1)
+const promiseLane = new PromiseLane(1);
 
-const w1 = promiseLane.add(p1)
-const w2 = promiseLane.add(p2)
+const w1 = promiseLane.add(p1);
+const w2 = promiseLane.add(p2);
 
-w1.then((v) => console.log(v))
-w2.then((v) => {
-    console.log(v);
-    promiseLane.stop()
-})
+w1.then((v) => console.log(v));
+w2.then((v) => console.log(v));
