e660c925ad7c74a2dbd7953711eed68a2ecb3097
VMois <vmois@protonmail.com>
2023-11-17 01:18:04 +0100

    Add JSDocs to the PromiseLane class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-// TODO: Add JSDocs
+/**
+ * @typedef {function(): Promise<any>} PromiseFactory
+ */
 
+/**
+ * Represents a class that manages the execution of promises with concurrency control.
+ */
 export default class PromiseLane {
+
+    /**
+     * Initializes an instance of PromiseLane
+     * @param {number} concurrency - The maximum number of promises that can be executed concurrently.
+     */
     constructor(concurrency) {
         this.concurrency = concurrency;
         this.current = 0
@@ -11,6 +21,11 @@ export default class PromiseLane {
         this.boundRun = this._run.bind(this);
     }
 
+    /**
+     * Adds a promise factory to the queue to be executed as soon as possible.
+     * @param {PromiseFactory} factory - A function that, when called, returns a Promise to be executed.
+     * @returns {Promise} a Promise that settles when the promise generate by a factory executes eventually.
+     */
     add(factory) {
         return new Promise((resolve, reject) => {
             this.queue.push([resolve, reject, factory])
@@ -18,6 +33,10 @@ export default class PromiseLane {
         })
     }
 
+    /**
+     * Executes promises from the queue based on the concurrency limit.
+     * @private
+     */
     _run() {
         while (this.current < this.concurrency && this.queue[0] !== undefined) {
             let [resolve, reject, factory] = this.queue.shift();
